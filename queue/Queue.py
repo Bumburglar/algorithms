cf230eb713aa3efd9918d2bbe9d51cbcb828291e
Bumburglar <jon08192@gmail.com>
2016-07-26 21:58:49 -0500

    Adding queue

---
+++ queue/Queue.py
@@ -0,0 +1,20 @@
+# Queue class for generic queueing
+
+
+class Queue:
+
+    def __init__(self):
+        self.items = []
+        return self.items
+
+    def enqueue(self, item):
+        self.items.insert(0, item)
+
+    def dequeue(self):
+        return self.items.pop()
+
+    def size(self):
+        return len(self.items)
+
+    def isEmpty(self):
+        return self.items == []
