a315ece7b513734f54e49116a186ad693fd8e281
Bumburglar <jon08192@gmail.com>
2016-08-26 00:28:50 -0500

    Fraction class with completed methods

--- fraction/fraction.py
+++ fraction/fraction.py
@@ -7,6 +7,20 @@ class Fraction():
         return str(self.num) + "/" + str(self.denum)
 
     def __add__(self, otherFraction):
-        newNum = self.num + otherFraction.num
-        newDenum = self.denum + otherFraction.denum
-        return str(newNum) + "/" + str(newDenum)
+        newNum = self.num * otherFraction.denum + otherFraction.num * self.denum
+        newDenum = self.denum * otherFraction.denum
+        common = self.gcd(newNum, newDenum)
+        return str(newNum//common) + "/" + str(newDenum//common)
+
+    def __eq__(self, other):
+        firstnum = self.num * other.denum
+        secondnum = other.num * self.denum
+        return firstnum == secondnum
+
+    def gcd(self, m, n):
+        while m % n != 0:
+            oldm = m
+            oldn = n
+            m = oldn
+            n = oldm % oldn
+        return n
