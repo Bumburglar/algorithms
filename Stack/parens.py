a864ad9384099edfda487ebc9a95724f3a59056a
Bumburglar <jon08192@gmail.com>
2016-08-28 21:01:15 -0500

    added balanced parens code

--- Stack/parens.py
+++ Stack/parens.py
@@ -7,14 +7,26 @@ def parens(parseString):
     index = 0
     while index < len(parseString) and balanced:
         symbol = parseString[index]
-    if symbol == '(':
-        s.push(symbol)
-    else:
-        if s.isEmpty():
-            balanced = False
+        if symbol in '[{(':
+            s.push(symbol)
         else:
-            s.pop()
+            if s.isEmpty():
+                balanced = False
+            else:
+                top = s.pop()
+                if not matches(top, symbol):
+                    balanced = False
+        index += 1
     if balanced and s.isEmpty():
         return True
     else:
         return False
+
+
+def matches(open, close):
+    opener = '[{('
+    closer = ']})'
+    return opener.index(open) == closer.index(close)
+
+print(parens("()()()()()()())))((())()))"))
+print(parens("(((())()))"))
